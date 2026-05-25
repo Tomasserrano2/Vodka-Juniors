de13475c490415799a2116c6603873cf16600175
Tomas <tomiserra2004@gmail.com>
2026-05-25 13:01:10 +0200

    Solved some things

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   projectId: "vodka-juniors",
   storageBucket: "vodka-juniors.firebasestorage.app",
   messagingSenderId: "836406435815",
-  appId: "1:836406435815:web:0b10e00b5cc635a8d82742"
+  appId: "1:836406435815:web:0b10e00b5cc635a8d82742",
 };
 
 const app = initializeApp(firebaseConfig);
